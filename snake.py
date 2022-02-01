b402d39b3a128b6c77202d7bc83347a39fb7bc5e
webala <webala1001@gmail.com>
2022-02-01 11:30:39 +0300

    initital commit

---
+++ snake.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame.locals import *
+import time
+
+
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()  # load image
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5)) #clear screen
+        self.parent_screen.blit(self.block, (self.x, self.y)) #draw the block on the surface
+        pygame.display.flip() #update surface
+
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def move_down(self):
+        self.y += 10
+        self.draw()
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((500, 500))
+        self.surface.fill((110, 110, 5))  # change background
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+
+    def run(self):
+        running = True
+
+        while running:  # Event loop
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:  # from pygame.locals
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+
+            self.snake.walk()
+            time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
+
+
+
+
